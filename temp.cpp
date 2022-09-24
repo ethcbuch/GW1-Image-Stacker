08c697a94d2876fcf2cc6a159eceffca2260aa4a
Hunter Johnson <hunmjohn@ut.utm.edu>
2022-09-24 12:32:57 -0500

    Heres this

--- temp.cpp
+++ temp.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -8,26 +9,73 @@ using namespace std;
 string makeName(string file, int n)
 {
   string name;
-  name = file + "/" + file + "_00" + n + ".ppms";
+  if(n < 10)
+    name = "ppms/" + file + "/" + file + "_00" + to_string(n) + ".ppm";
+  else
+    name = "ppms/" + file + "/" + file + "_0" + to_string(n) + ".ppm";
   return name;
 }
+
+struct pixel {
+  int red;
+  int blue;
+  int green;
+};
+
+pixel operator+(pixel a, pixel b)
+{
+  struct pixel p1;
+  p1.red = a.red + b.red;
+  p1.green = a.green + b.green;
+  p1.blue = a.blue + b.blue;
+  return p1;
+}
+
+void getPixelData(string file, n) //Can just use private member 
+{
+  struct pixel p1;
+  struct pixel p2;
+  ifstream inputFile;
+  string name = makeName(file, n);
+  string magic_number;
+  int col,row,max;
+  vector<vector<pixel>> pixels;
+  inputFile.open(name);
+  inputFile >> magic_number >> col >> row >> max;
+  pixels.resize(col, vector<pixel> (row));
+  for(int r = 0; r < row; r++)
+    {
+      
+    }
+
+
+
+  
+  for(int r = 0; r < row; r++)
+    {
+      for(int c = 0; c < col; c++)
+	{
+	  inputFile >> p1.red >> p1.green >> p1.blue;
+	  pixels[r][c] = pixels[r][c] + p1;
+	}
+    }
+  inputFile.close();
+}
+  
+
+
+
 
 
 int main()
 {
   string file;
   cin >> file;
-  for(int i = 1; i < 11; i++)
-    {
-      file = makeName(file, i);
-      cout <<  file << endl;
-    }
-  return 0;
-}
-
+  getPixelData(file);
 
 
 
 
   
+  return 0;
 }
